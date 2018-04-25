4f37cc467210be66e0b386566ee84a993938cb5b
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2018-04-25 17:13:26 +0200

    comment on netflix input data structure

--- netflix-prep.py
+++ netflix-prep.py
@@ -5,10 +5,31 @@ import getopt
 import argparse
 import sys
 
-def netflix_to_vw(loc_txt, loc_output, train=True):
+TRAIN = True
+PATH = 'netflix/train_small'
+
+def netflix_to_vw(loc_txt, loc_output, train):
     """
-    Munges a TXT file (loc_txt) to a VW file (loc_output). Set "train"
-    to False when munging a test set.
+    Munges a collection of TXT files from a path into a single VW file (loc_output). 
+    Set "TRAIN" to False when munging a test set (can be done with -t cli flag).
+
+    loc_text should point to a folder containing a collection of text files.
+    Each text file should contain all ratings for a single movie. 
+    
+    First line contains the movie ID. 
+
+    Each rating contains 3 columns: userid:score:date
+
+    Example for movie id 20:
+
+    20:
+    2625420,2,2004-06-03
+    1508073,3,2004-12-02
+    1371016,3,2005-05-26
+    1374336,4,2004-09-26
+    1254903,3,2005-06-15
+    ...
+
     """
     start = datetime.now()
     print("\nTurning %s into %s. Is_train_set? %s"%(loc_txt,loc_output,bool(train)))
@@ -16,6 +37,9 @@ def netflix_to_vw(loc_txt, loc_output, train=True):
     """
     TODO: Process
     """
+
+    with open(loc_output,"w") as outfile:
+        1+1
 
 
 def is_number(s):
